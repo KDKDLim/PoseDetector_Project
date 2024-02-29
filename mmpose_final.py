8f0ff1aa2e965496676faedcc735d79866547ab2
dhakdldl <161277520+dhakdldl@users.noreply.github.com>
2024-02-29 14:08:18 +0900

    Add files via upload

---
+++ mmpose_final.py
@@ -0,0 +1,68 @@
+import mmcv
+from mmcv import imread
+import mmengine
+from mmengine.registry import init_default_scope
+import numpy as np
+
+from mmpose.apis import inference_topdown
+from mmpose.apis import init_model as init_pose_estimator
+from mmpose.evaluation.functional import nms
+from mmpose.registry import VISUALIZERS
+from mmpose.structures import merge_data_samples
+from mmdet.apis import inference_detector, init_detector
+
+img = 'C:/Users/user/Downloads/mmpose-main (1)/mmpose-main/.venv/test/uploads/26.jpg'
+
+def visualize_img(img_path):
+    pose_config = 'C:/Users/user/Downloads/mmpose-main (1)/mmpose-main/configs/body_2d_keypoint/topdown_heatmap/coco/td-hm_hrnet-w32_8xb64-210e_coco-256x192.py'
+    pose_checkpoint = 'https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w32_coco_256x192-c78dce93_20200708.pth'
+    det_config = 'C:/Users/user/Downloads/mmpose-main (1)/mmpose-main/demo/mmdetection_cfg/faster_rcnn_r50_fpn_coco.py'
+    det_checkpoint = 'https://download.openmmlab.com/mmdetection/v2.0/faster_rcnn/faster_rcnn_r50_fpn_1x_coco/faster_rcnn_r50_fpn_1x_coco_20200130-047c8118.pth'
+    device = 'cpu'
+
+
+    detector = init_detector(
+        det_config,
+        det_checkpoint,
+        device=device
+    )
+
+    pose_estimator = init_pose_estimator(
+        pose_config,
+        pose_checkpoint,
+        device=device,
+    )
+
+    pose_estimator.cfg.visualizer.radius = 3
+    pose_estimator.cfg.visualizer.line_width = 1
+    visualizer = VISUALIZERS.build(pose_estimator.cfg.visualizer)
+    visualizer.set_dataset_meta(pose_estimator.dataset_meta)
+
+    # predict bbox
+    scope = detector.cfg.get('default_scope', 'mmdet')
+    if scope is not None:
+        init_default_scope(scope)
+    detect_result = inference_detector(detector, img_path)
+    pred_instance = detect_result.pred_instances.cpu().numpy()
+    bboxes = np.concatenate(
+        (pred_instance.bboxes, pred_instance.scores[:, None]), axis=1)
+    bboxes = bboxes[np.logical_and(pred_instance.labels == 0,
+                                   pred_instance.scores > 0.3)]
+    bboxes = bboxes[nms(bboxes, 0.3)][:, :4]
+
+    # predict keypoints
+    pose_results = inference_topdown(pose_estimator, img_path, bboxes)
+    data_samples = merge_data_samples(pose_results)
+
+    # show the results
+    img = mmcv.imread(img_path, channel_order='rgb')
+
+    mean_scores = np.mean(data_samples.pred_instances.keypoint_scores, axis=1)
+    indices = np.where(mean_scores >= 0.43)[0]
+    filtered_instances = data_samples.pred_instances[indices]
+    point = filtered_instances.keypoints
+    box_point = filtered_instances.bboxes
+    return point, box_point
+
+#ex_result = visualize_img(img)
+#print(ex_result)
